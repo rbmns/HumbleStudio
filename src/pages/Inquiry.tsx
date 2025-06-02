5ac2ef30c5a7c363ebafdd2d02efe137328117d1
rbmns <r.a.biemans@gmail.com>
2025-06-02 16:00:40 +0200

    Update Inquiry.tsx

--- src/pages/Inquiry.tsx
+++ src/pages/Inquiry.tsx
@@ -47,10 +47,8 @@ const Inquiry = () => {
       <div className="container mx-auto px-4 md:px-6 py-16 md:py-24">
         <h1 className="text-3xl md:text-4xl font-bold mb-8 text-center text-white">HumbleStudio Inquiry Form</h1>
         
-        <div className="max-w-4xl mx-auto mb-16">
-        <iframe data-tally-src="https://tally.so/embed/n9yYNV?alignLeft=1&hideTitle=1&transparentBackground=1&dynamicHeight=1" loading="lazy" width="100%" height="200" frameborder="0" marginheight="0" marginwidth="0" title="Curious?"></iframe>
+      <iframe data-tally-src="https://tally.so/embed/n9yYNV?alignLeft=1&hideTitle=1&transparentBackground=1&dynamicHeight=1" loading="lazy" width="100%" height="200" frameborder="0" marginheight="0" marginwidth="0" title="Curious?"></iframe>
 <script>var d=document,w="https://tally.so/widgets/embed.js",v=function(){"undefined"!=typeof Tally?Tally.loadEmbeds():d.querySelectorAll("iframe[data-tally-src]:not([src])").forEach((function(e){e.src=e.dataset.tallySrc}))};if("undefined"!=typeof Tally)v();else if(d.querySelector('script[src="'+w+'"]')==null){var s=d.createElement("script");s.src=w,s.onload=v,s.onerror=v,d.body.appendChild(s);}</script>
-        </div>
       </div>
       <Footer />
     </div>
